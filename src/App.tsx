78a5b25ada8d2e8fe1918a3d0d4e373e63e9fe84
Zoe de Moffarts <zoe.demoffarts@gmail.com>
2020-01-12 15:54:38 +0100

    Setup Apollo Client

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useHistory,
+} from 'react-router-dom'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
 import Login from './pages/Login'
 import Header from './components/Header'
 import MaterialSetup from './utils/MaterialSetup'
 import { UserContextProvider } from './utils/UserContext'
 
 const App = () => {
+  const client = new ApolloClient({
+    request: operation => {
+      const token = localStorage.getItem('token')
+      operation.setContext({
+        headers: {
+          authorization: token ? `Bearer ${token}` : '',
+        },
+      })
+    },
+    uri: `${process.env.REACT_APP_GRAPHQL_ENDPOINT}`,
+  })
   return (
     <MaterialSetup>
       <UserContextProvider>
